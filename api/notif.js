01a7e5a394bba2517e807b79ff9e0c8a6b97367f
Silc <159597511+1Silc@users.noreply.github.com>
2025-05-24 00:18:05 -0400

    Delete api directory

--- api/notif.js
+++
@@ -1,7 +0,0 @@
-const urlString = window.location.href;
-const url = new URL(urlString);
-const params = new URLSearchParams(url.search);
-const msgParameter = params.get('msg');
-async function notif() {
-  alert(msgParameter)
-}
