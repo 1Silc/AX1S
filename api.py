f83b8f858f9391a8c05e6842143cb8fbb6a69b8a
Silc <159597511+1Silc@users.noreply.github.com>
2025-07-12 00:12:53 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,71 +1,74 @@
-from flask import Flask, request, jsonify, Response, make_response
+from flask import Flask, request, Response, make_response
 
 app = Flask(__name__)
 
 @app.route('/list', methods=['GET'])
-def list():
+def list_ids():
     try:
         with open('ban-list.txt', 'r') as file:
             lines = [line.strip() for line in file]
-
-        # Join with newlines and return as plain text
         content = '\n'.join(lines)
         return Response(content, mimetype='text/plain')
-
     except FileNotFoundError:
         return Response("ban-list.txt not found.", mimetype='text/plain')
 
-        
 @app.route('/check', methods=['GET'])
 def check():
-    id = request.headers.get('X-User-ID')
-    with open('ban-list.txt', 'r') as file:
-        file_content = file.read()
-        if id in file_content:
-            response = make_response('True')
-            response.headers['X-Is-Listed'] = 'True'
-            response.status_code = 200
-        else:
-            response2 = make_response('False')
-            response2.headers['X-Is-Listed'] = 'False'
-            response2.status_code = 404
+    user_id = request.headers.get('X-User-ID')
+    if not user_id:
+        return "Missing X-User-ID header", 400
+
+    try:
+        with open('ban-list.txt', 'r') as file:
+            file_content = file.read()
+            if user_id in file_content:
+                response = make_response('True')
+                response.headers['X-Is-Listed'] = 'True'
+                response.status_code = 200
+                return response
+            else:
+                response = make_response('False')
+                response.headers['X-Is-Listed'] = 'False'
+                response.status_code = 404
+                return response
+    except FileNotFoundError:
+        return "ban-list.txt not found.", 500
+
 @app.route('/add', methods=['GET'])
 def add():
-    tkn = request.headers.get('X-Admin-Token')
-    id = request.headers.get('X-User-ID')
-    if tkn == "QkVJTkcgR0FZIElTIE9LQVkgfCBCRUlORyBHQVkgSVMgT0tBWSB8IEJFSU5HIEdBWSBJUyBPS0FZIHwgQkVJTkcgR0FZIElTIE9LQVkgfCBCRUlORyBHQVkgSVMgT0tBWSB8IEJFSU5HIEdBWSBJUyBPS0FZIHwgQkVJTkcgR0FZIElTIE9LQVkgfCBCRUlORyBHQVkgSVMgT0tBWSB8IEJFSU5HIEdBWSBJUyBPS0FZ":
+    token = request.headers.get('X-Admin-Token')
+    user_id = request.headers.get('X-User-ID')
 
-        if not new_entry:
-            return "No entry provided", 400
+    if not user_id:
+        return "Missing X-User-ID header", 400
 
+    if token == "QkVJTkcgR0FZIElTIE9LQVkgfCBCRUlORyBHQVkgSVMgT0tBWSB8IEJFSU5HIEdBWSBJUyBPS0FZIHwgQkVJTkcgR0FZIElTIE9LQVkgfCBCRUlORyBHQVkgSVMgT0tBWSB8IEJFSU5HIEdBWSBJUyBPS0FZIHwgQkVJTkcgR0FZIElTIE9LQVkgfCBCRUlORyBHQVkgSVMgT0tBWSB8IEJFSU5HIEdBWSBJUyBPS0FZ":
         with open('ban-list.txt', 'a') as file:
-            file.write(id + '\n')
+            file.write(user_id + '\n')
         return "Added ID to ban-list."
     else:
-        return "Wrong token."
+        return "Wrong token.", 403
 
 @app.route('/remove', methods=['GET'])
 def remove():
-    tkn = request.headers.get('X-Admin-Token')
-    if tkn == "QkVJTkcgR0FZIElTIE9LQVkgfCBCRUlORyBHQVkgSVMgT0tBWSB8IEJFSU5HIEdBWSBJUyBPS0FZIHwgQkVJTkcgR0FZIElTIE9LQVkgfCBCRUlORyBHQVkgSVMgT0tBWSB8IEJFSU5HIEdBWSBJUyBPS0FZIHwgQkVJTkcgR0FZIElTIE9LQVkgfCBCRUlORyBHQVkgSVMgT0tBWSB8IEJFSU5HIEdBWSBJUyBPS0FZ":
-        target = request.headers.get('X-User-ID')
-    
-        if not target:
-            return "No target provided", 400
-    
-        cleaned_lines = []
-    
-        with open('ban-list.txt', 'r') as file:
-            for line in file:
-                if target not in line:
-                    cleaned_lines.append(line.strip())
-    
-        # Optionally write back the cleaned list
-        with open('ban-list.txt', 'w') as file:
-                for line in cleaned_lines:
-                file.write(line + '\n')
-    
-        return "Removed ID from ban-list."
+    token = request.headers.get('X-Admin-Token')
+    user_id = request.headers.get('X-User-ID')
+
+    if not user_id:
+        return "Missing X-User-ID header", 400
+
+    if token == "QkVJTkcgR0FZIElTIE9LQVkgfCBCRUlORyBHQVkgSVMgT0tBWSB8IEJFSU5HIEdBWSBJUyBPS0FZIHwgQkVJTkcgR0FZIElTIE9LQVkgfCBCRUlORyBHQVkgSVMgT0tBWSB8IEJFSU5HIEdBWSBJUyBPS0FZIHwgQkVJTkcgR0FZIElTIE9LQVkgfCBCRUlORyBHQVkgSVMgT0tBWSB8IEJFSU5HIEdBWSBJUyBPS0FZ":
+        try:
+            with open('ban-list.txt', 'r') as file:
+                lines = [line.strip() for line in file if user_id not in line]
+            with open('ban-list.txt', 'w') as file:
+                for line in lines:
+                    file.write(line + '\n')
+            return "Removed ID from ban-list."
+        except FileNotFoundError:
+            return "ban-list.txt not found.", 500
+    else:
+        return "Wrong token.", 403
 
 if __name__ == '__main__':
     app.run(debug=True)
